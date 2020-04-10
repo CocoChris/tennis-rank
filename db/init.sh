fe6b5f4709887e2d9bffd797ebbb1a50aa582a79
JesseMK <jessewangzh@hotmail.com>
2020-04-10 01:04:01 -0400

    add init.sh for db

--- db/init.sh
+++ db/init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
 USE ita;
-source *.sql;
+source current_phase.sql;
+source event_info.sql;
+source event_level.sql;
+source grade_record.sql;
+source player_info.sql;
+source pts_record.sql;
+source score_board.sql;
 
 EOF
